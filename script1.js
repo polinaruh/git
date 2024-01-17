71d27f83aa4ec656081f978cab687828a375b4b9
Poina <polina29ruhmakova@gmail.com>
2024-01-18 01:07:16 +0300

    new commit

---
+++ script1.js
@@ -0,0 +1,572 @@
+"use strict"
+alert('Привет, это моя первая программа'); // комментарий 
+/* комментарий 2 */
+// let num;
+// num = 123;
+// alert(num);
+// let z, j, l;
+// z = 1;
+// j = 2;
+// l = 3;
+// let w = 1, g = 2, k = 3;
+// let s = 1;
+// let f = 2;
+// let h = 7;
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+// let a = 1 + 2 + 3;
+// alert(a);
+
+// let a = 10, b = 2;
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+
+
+// let c = 10, d = 5;
+// let result = c + d;
+// alert(result);
+
+// let a = 1, b = 2, c = 3;
+// alert(a + b + c);
+
+
+// let a = 10, b = 5;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert(result);
+
+// let a = 1.5, b = 0.75;
+// alert(a + b);
+
+// let a1 = -100;
+// alert(a1);
+
+// let a2 = -5;
+// alert(-a2);
+
+// let a = 13;
+// let b = 5;
+// alert(a % b);
+
+// let a3 = 2;
+// alert(a3 ** 10);
+
+// let str1 = 'Анна';
+// let str2 = 'Елфимова';
+// alert(str1);
+// alert(str2);
+
+// let str = '!!!';
+// alert(str);
+
+// let str3 = 'java';
+// let str4 = 'script';
+// alert(str3 + str4);
+
+// let str5 = 'hello';
+// let str6 = 'world';
+// alert(str5 + ' ' + str6);
+
+// let str7 = 'fffkkklll;;;';
+// alert(str7.length);
+
+// let str1 = 'xxx';
+// let str2 = 'yyy';
+// let txt  = `aaa ${str1} bbb ${str2} ccc`;
+// alert(txt);
+
+// let str = `a
+// b
+// c`;
+// alert(str);
+
+// let ff;
+// alert(ff);
+
+// let gg = null;
+// alert(gg);
+
+// let t = true;
+// alert(t);
+
+// let f1 = false;
+// alert(f1);
+
+// let str8 = 'hello';
+// let str9 = 'world';
+// alert(str8 * str9);
+
+// alert(10 / 0);
+// alert(-10 / 0);
+
+// let num = 110;
+// console.log(num);
+
+// let num1 = 123;
+// let num2 = 456;
+// let num3 = 789;
+// console.log(num1, num2, num3);
+
+// console.log(123);
+// console.log('123');
+// console.log(gg);
+// console.log(t);
+// console.log(f1);
+// console.log(str8 * str9);
+
+// alert(eee);
+
+// let r = 15;
+// const PI = 3.14;
+// alert(r * 2 * PI);
+
+// let a = 10, b = 20;
+// alert( Number(a) + Number(b) );
+
+// let a = +'2';
+// let b = +'3';
+// alert(a + b); 
+
+// let n = '5px', n2 = '6px';
+// alert(parseInt(n) + parseInt(n2));
+
+// let n1  = '5.5px', n3 = '6.25px';
+// alert(parseFloat(n1) + parseFloat(n3));
+
+// let n4  = '5.5px', n5 = '6.25px';
+// alert(parseFloat(n4) + parseFloat(n5) + 'px');
+
+// let m = 123, mm = 123;
+// alert(String(m) + String(mm));
+
+// let r1 = 10023;
+// alert(String(r1).length);
+
+// let r2 = 10023, r3 = 11110;
+// alert(String(r2).length + String(r3).length);
+
+// let text = 'abcde';
+// alert(text[0] + text[2] + text[4]);
+
+// let text1 = text[4] + text[3] + text[2] + text[1] + text[0];
+// alert(text1);
+
+// let num = 3, str = 'abcde';
+// alert(str[num]);
+
+// let stro = 'abcde';
+// alert(stro[stro.length - 1]);
+// alert(stro[stro.length - 2]);
+// alert(stro[stro.length - 3]);
+
+// let stroka = '12345';
+// alert(Number(stroka[0]) + Number(stroka[1]) + Number(stroka[2]) + Number(stroka[3]) + Number(stroka[4]));
+
+// let n = String(12345);
+// alert(Number(n[0]) + Number(n[1]) + Number(n[2]) + Number(n[3]) + Number(n[4]));
+
+// alert(n[0] * n[1] * n[2] * n[3] * n[4]);
+
+// let nn = n[4] + n[3] + n[2] + n[1] + n[0];
+// alert(nn);
+
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+
+// let num = 10;
+// num++;
+// num++;
+// num--;
+// alert(num);
+
+// alert(0.1 * 0.2);
+
+// alert(0.3 - 0.1);
+
+// alert('Вам ' + prompt('Сколько вам лет?') + ' лет' );
+
+// let num1 = prompt('Введите первое число');
+// let num2 = prompt('Введите второе число');
+
+// alert(Number(num1) + Number(num2)); 
+
+// let kv = prompt('Ведите длину стороны квадрата (см)');
+// alert('Площадь квадрата равна ' + kv * kv + ' см');
+
+// let a = prompt('Ведите длину первой стороны прямоугольника (см)');
+// let b = prompt('Введите длину второй стороны прямоугольника (см)');
+// alert('Периметр прямоугольника равен ' + (Number(a) + Number(b)) * 2  + ' см');
+
+// document.write('sometext');
+// document.write('<i>worldwide</i>');
+// let str = 'text';
+// document.write('<i>' + str + '</i>');
+// document.write('1<br>');
+// document.write('2<br>');
+// document.write('3<br>');
+// document.write('4<br>');
+// document.write('5<br>');
+
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ' + (num1 + num2));
+
+// let a = 1;
+// let b = 2;
+// console.log(a + b);
+
+// let num = '123';
+// let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+// console.log(sum);
+
+// let num = String(123);
+// console.log(num[0]);
+
+// let a = 0;
+// console.log(++a);
+
+// let num = 123;
+// console.log(String(num).length);
+
+// let a = 24 * 60 * 60;
+// console.log(a);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length - 1]);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length - 1]);
+
+// let a = '123';
+// let b = '456';
+// let s = Number(a) + Number(b);
+// console.log(s);
+
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+
+// console.log(aaa + bbb + ccc);
+
+// let s = 60 * 60 * 24;
+// alert(s);
+
+// let s1 = 60 * 60 * 24 * 30;
+// alert(s1);
+
+// let s2 = 60 * 60 * 24 * 365;
+// alert(s2);
+
+// let s3 = 60 * 24;
+// alert(s3);
+
+// let s4 = 365 * 60 * 24;
+// alert(s4);
+
+// let b = 1024 * 1024;
+// alert(b);
+
+// let b1 = 1024 * 1024 * 1024;
+// alert(b1);
+
+// let b2 = 1024 * 1024 * 1024 * 10;
+// alert(b2);
+
+// let b3 = 1024 * 1024 * 1024 * 1024;
+// alert(b3);
+
+// let t = 1024 * 1024 * 1024;
+// alert(t);
+
+
+// Пусть дана переменная r с радиусом круга. По соответствующей формуле найдите площадь круга и запишите ее в переменную s. Выведите содержимое этой переменной на экран.
+
+// let r = 5;
+// const PI = 3.14;
+// let s = PI * r ** 2;
+// alert(s);
+
+// Пусть дана переменная a со стороной квадрата. Найдите площадь квадрата и запишите ее в переменную s. Выведите содержимое этой переменной на экран.
+
+// let a = 2;
+// let s = a * a;
+// alert(s);
+
+// Пусть даны переменные a и b со сторонами прямоугольника. Найдите площадь прямоугольника и запишите ее в переменную s. Выведите содержимое этой переменной на экран.
+
+// let a = 10, b = 3;
+// let s = a * b;
+// alert(s);
+
+// Пусть даны переменные a и b со сторонами прямоугольника. Найдите периметр прямоугольника и запишите его в переменную p. Выведите содержимое этой переменной на экран.
+
+// let a = 10, b = 3;
+// let p = (a + b) * 2;
+// alert(p);
+
+// Пусть дана переменная tc с температурой в градусах Цельсия. По соответствующей формуле выполните перевод этой температуры в градусы Фаренгейта.
+
+// let tc = 20;
+// let tf = tc * (9/5) + 32;
+// alert(tf);
+
+// Пусть дана переменная tf с температурой в градусах Фаренгейта. По соответствующей формуле выполните перевод этой температуры в градусы Цельсия.
+
+// let tf = 20;
+// let tc = (tf -32) * (5/9);
+// alert(tc);
+
+// let arr = [1, 2, 3, 4, 5];
+// alert(arr);
+// console.log(arr);
+
+// let arr = ['1', 'a', 'b'];
+// alert(arr);
+// console.log(arr);
+
+// let arr = [1, 2, 3];
+// alert(arr[0]);
+// alert(arr[1]);
+// alert(arr[2]);
+
+
+// let arr = [1, 2, 3];
+// alert(arr[0]);
+// alert(arr[1]);
+// alert(arr[2]);
+
+// let arr = [1, 2, 3];
+// alert(arr[0] + arr[1] + arr[2]);
+
+// let arr = ['a', 'b', 'c', 'd'];
+// alert(arr[0] + '+' + arr[1] + '+' + arr[2] + '+' + arr[3]);
+
+// let arr1 = ['3', 'bb', 'v'];
+// console.log(arr1.length); 
+
+// let arr2 = ['ff', '1', '2'];
+// console.log(arr2[arr2.length - 1]); 
+
+// let arr = ['a', 'b', 'c'];
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+// alert(arr);
+
+// let arr = [1, 2, 3];
+// arr[0] += 3;
+// arr[1] += 3;
+// arr[2] += 3;
+// alert(arr);
+
+
+// let arr = [1, 2, 3];
+// arr[0]++;
+// arr[1]++;
+// arr[2]++;
+// alert(arr);
+
+// let arr1 = [];
+// arr1[0] = 1;
+// arr1[1] = 2;
+// arr1[2] = 3;
+// alert(arr1);
+
+// let arr = [1, 2, 3];
+// arr[3] = 4;
+// arr[4] = 5;
+// alert(arr);
+
+// let arr = [];
+// arr[3] = 'a';
+// arr[8] = 'b';
+// alert(arr.length);
+
+// let arr1 = [];
+// arr1.push(1);
+// arr1.push(2);
+// arr1.push(3);
+// alert(arr1);
+
+// let arr = [1, 2, 3];
+// arr.push(4);
+// arr.push(5);
+// alert(arr);
+
+// let arr = ['a', 'b', 'c'];
+// let key = 2;
+// alert(arr[key]);
+
+// let arr = [1, 2, 3, 4, 5];
+// let key1 = 1;
+// let key2 = 2;
+// alert(arr[key1] + arr[key2]);
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// delete arr[1];
+// delete arr[2];
+// alert(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[arr.length - 1]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.lenght);
+
+// let obj = {1: 'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'};
+// alert(obj[1]);
+// alert(obj[2]);
+// alert(obj[3]);
+// alert(obj[4]);
+// alert(obj[5]);
+// alert(obj[6]);
+// alert(obj[7]);
+
+// let obj = {1: 'январь', 2: 'февраль', 3: 'март', 4: 'апрель', 5: 'май', 6: 'июнь', 7: 'июль', 8: 'август', 9: 'сентябрь', 10: 'октябрь', 11: 'ноябрь', 12: 'декабрь'};
+// console.log(obj);        
+
+// let user = {name: 'Иван' , surname: 'Иванченко', patronymic: 'Олегович'};
+// alert(user['surname'] + ' ' + user['name'] + ' ' + user['patronymic']);
+
+// let date  = {year: '2024' , month: 'январь', day: '14'};
+// alert(date.year + '-' + date.month + '-' + date.day); 
+
+// let obj = {
+// 	'1a': 1,
+// 	'b2': 2,
+// 	'c-c': 3,
+// 	'd 4': 4,
+// 	'e5': 5
+// };
+
+// console.log(obj['1a']);
+// console.log(obj.b2);
+// console.log(obj['c-c']);
+// console.log(obj['d 4']);
+// console.log(obj.e5);
+
+// let obj = {a: 1, b: 2, c: 3};
+
+// obj['a'] = '!';
+
+// obj.b = '!';
+
+// console.log(obj);
+
+// let obj = {x: 1, y: 2, z: 3};
+
+// obj.x = obj.x ** 2;
+// obj.y = obj.y ** 2;
+// obj.z = obj.z ** 2;
+
+// console.log(obj);
+
+
+// let obj = {};
+// obj.a = 1;
+// obj.b = 2;
+// obj.c = 3;
+// obj.d = 4;
+// obj.e = 5;
+// console.log(obj);
+
+// let obj = {11: 'a', 25: 'b', 3: 'c'};
+
+// console.log(obj[11]); 
+// console.log(obj[25]); 
+// console.log(obj[3]); 
+
+// let obj = {3: 'c', 11: 'a', 25: 'b'};
+
+// console.log(obj[11]);
+// console.log(obj[25]); 
+// console.log(obj[3]); 
+
+// let obj = {x: 1, y: 2, z: 3};
+// let keys = Object.keys(obj);
+// console.log(keys);
+
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(Object.keys(obj).length); 
+
+// let obj = {x: 1, key: 2, z: 3};
+// let key = 'x';
+// alert(obj[key]);
+
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(obj['x']);
+
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+// console.log(obj[key]);
+
+// let obj = {x: 1, y: 2, z: 3};
+// let prop = 'x';
+// console.log(obj[prop]);
+
+// let obj = {x: 1, y: 2, z: 3};
+// let prop = 'x';
+// console.log(obj[prop]);
+
+// let key = 'x';
+// let obj = {
+// 	[key]: 1,
+// 	y: 2,
+// 	z: 3
+// };
+
+
+// let key1 = 'x';
+// let key2 = 'y';
+// let key3 = 'z';
+
+// let obj = {
+// 	[key1]: 1,
+// 	[key2]: 2,
+// 	[key3]: 3
+// };
+// console.log(obj);
+
+// const arr = [1, 2, 3, 4, 5];
+// const res = arr[1] + arr[2];
+// console.log(res);
+
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(obj['x']);
+
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+// console.log(obj[key]);
+
+// let obj = {x: 1, y: 2, z: 3};
+// let sum = obj['x'] + obj['y'] + obj['z'];
+// console.log(sum);
+
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(Object.keys(obj).length);
